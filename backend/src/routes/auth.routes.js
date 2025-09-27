b54c0f7d98cdd88c19e86fe4d0b04370a77e161d
Palakkasaudhan <Palakkasaudhan123@gamil.com>
2025-09-27 22:24:49 +0530

    registeruser and login  created

--- backend/src/routes/auth.routes.js
+++ backend/src/routes/auth.routes.js
@@ -1,9 +1,11 @@
 
 const express =require('express')
+const authcontroller =require("../controllers/auth.controller")
 
 const router =express.Router();
 
-router.post('./user/register',(req,res)=>{
+router.post('/user/register',authcontroller.registerUser)
+router.post('/user/login',authcontroller.loginUser)
     
-})
+
 module.exports =router;
